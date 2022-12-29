5639891ad3c96ec3a9d4ab1a2904b7aea0d87beb
Gabriel Noya <gabrielnoya95@gmail.com>
2022-12-28 22:20:53 -0400

    New feature: you can click the board and fill it with X (harcoded)

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -1,26 +1,33 @@
-import { TileIndex } from '../../models/tile.model'
+import { useState } from 'react'
+import { Player } from '../../models/player.model'
+import { play } from '../../models/board.model'
+import { makeEmptyTiles, Tile, TileIndex } from '../../models/tile.model'
 import BoardTile from '../board-tile/board-tile.component'
 import styles from './board.component.module.css'
 
 interface BoardProps {}
 
 export default function Board(props: BoardProps) {
-  const {} = props
+  const [tiles, setTiles] = useState<Tile[]>(makeEmptyTiles())
+  const [player] = useState<Player>('X')
 
-  const onTileClick = (index: TileIndex) => console.log(index)
+  const onTileClick = (index: TileIndex) => {
+    setTiles((prevState: any) => {
+      const newTiles: Tile[] = play(prevState, player, index)
+      return newTiles
+    })
+  }
 
   return (
     <div className={styles.container}>
-      {Array(9)
-        .fill(null)
-        .map((_, index) => (
-          <BoardTile
-            key={index}
-            index={index as TileIndex}
-            filledBy={'empty'}
-            onClick={onTileClick}
-          />
-        ))}
+      {tiles.map((tile: Tile, index) => (
+        <BoardTile
+          key={index}
+          index={tile.index}
+          filledBy={tile.filledBy}
+          onClick={onTileClick}
+        />
+      ))}
     </div>
   )
 }
