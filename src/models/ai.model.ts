2c28cafda08f6a1b1cc0a80feac1d43b52310d3c
Gabriel Noya <gabrielnoya95@gmail.com>
2023-04-12 14:22:41 -0400

    Migrated from Create React App to Vite

--- src/models/ai.model.ts
+++ src/models/ai.model.ts
@@ -1,11 +1,11 @@
-import { getAvailableActions, getWinningPlayer, play } from './board.model'
-import { negatePlayer, Player } from './player.model'
-import { Tile, TileIndex } from './tile.model'
-import _ from 'lodash'
+import { getAvailableActions, getWinningPlayer, play } from './board.model';
+import { negatePlayer, Player } from './player.model';
+import { Tile, TileIndex } from './tile.model';
+import _ from 'lodash';
 
 export interface AIAction {
-  index: TileIndex
-  score: number
+  index: TileIndex;
+  score: number;
 }
 
 /*
@@ -13,25 +13,24 @@ export interface AIAction {
   This is calculated using the minimax algorithm.
 */
 export const getBestMove = (tiles: Tile[], player: Player): TileIndex => {
-  const availableActions = getAvailableActions(tiles)
-  if (availableActions.length === 9) return 4
+  const availableActions = getAvailableActions(tiles);
+  if (availableActions.length === 9) return 4;
 
-  let bestMove: TileIndex = 0
-  let bestScore: number = -Infinity
+  let bestMove: TileIndex = 0;
+  let bestScore: number = -Infinity;
 
   for (let action of availableActions) {
-    const clonedTiles = _.cloneDeep(tiles)
-    const moveScore = minimax(clonedTiles, action, player, 1, player)
-    console.log(moveScore)
+    const clonedTiles = _.cloneDeep(tiles);
+    const moveScore = minimax(clonedTiles, action, player, 1, player);
 
     if (moveScore > bestScore) {
-      bestMove = action
-      bestScore = moveScore
+      bestMove = action;
+      bestScore = moveScore;
     }
   }
 
-  return bestMove
-}
+  return bestMove;
+};
 
 /*
   Minimax algorithm maximizes the score of the original player and minimizes the score of the
@@ -44,63 +43,63 @@ export const minimax = (
   depth: number,
   originalPlayer: Player
 ): number => {
-  const newTiles = play(tiles, player, action)
-  let winningPlayer = getWinningPlayer(newTiles)
+  const newTiles = play(tiles, player, action);
+  let winningPlayer = getWinningPlayer(newTiles);
 
   // Game is finished because winningPlayer is the player who just played
   if (winningPlayer === player) {
-    return winningPlayer === originalPlayer ? 10000 / depth : -10000 / depth
+    return winningPlayer === originalPlayer ? 10000 / depth : -10000 / depth;
   }
 
   // Game tied
   if (winningPlayer === 'tie') {
-    return 0
+    return 0;
   }
 
   // Game is not finished
-  const availableActions = getAvailableActions(newTiles)
+  const availableActions = getAvailableActions(newTiles);
 
   // We maximize
   if (originalPlayer !== player) {
-    let bestScore: number = -Infinity
+    let bestScore: number = -Infinity;
 
     for (let action of availableActions) {
-      const clonedTiles = _.cloneDeep(newTiles)
+      const clonedTiles = _.cloneDeep(newTiles);
       const moveScore = minimax(
         clonedTiles,
         action,
         originalPlayer,
         depth + 1,
         originalPlayer
-      )
+      );
 
       if (moveScore > bestScore) {
-        bestScore = moveScore
+        bestScore = moveScore;
       }
     }
 
-    return bestScore
+    return bestScore;
   }
 
   // We minimize
   else {
-    let bestScore: number = Infinity
+    let bestScore: number = Infinity;
 
     for (let action of availableActions) {
-      const clonedTiles = _.cloneDeep(newTiles)
+      const clonedTiles = _.cloneDeep(newTiles);
       const moveScore = minimax(
         clonedTiles,
         action,
         negatePlayer(originalPlayer),
         depth + 1,
         originalPlayer
-      )
+      );
 
       if (moveScore < bestScore) {
-        bestScore = moveScore
+        bestScore = moveScore;
       }
     }
 
-    return bestScore
+    return bestScore;
   }
-}
+};
