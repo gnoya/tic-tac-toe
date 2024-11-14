3c9926148376965edddc85a667ae9c27448fff55
Gabriel Noya <gabrielnoya95@gmail.com>
2024-11-14 16:26:30 -0400

    Dark color scheme

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -18,7 +18,7 @@ export function ThemeToggler({ className }: ThemeTogglerProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className={cn(className)}>
+        <Button variant="default" size="icon" className={cn(className)}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Switch theme</span>
