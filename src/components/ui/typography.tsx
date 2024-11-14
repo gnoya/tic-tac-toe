137ee4986b9d354a291b18265398706996a14ea1
Gabriel Noya <gabrielnoya95@gmail.com>
2024-11-13 21:35:50 -0400

    Light color scheme

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -6,7 +6,7 @@ import { cn } from '../../lib/utils'
 const typographyVariants = cva('text-foreground', {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+      h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary',
       h2: 'scroll-m-20 text-3xl font-semibold tracking-tight',
       h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
       h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -6,7 +6,7 @@ export function Play() {
   useTitle('Play')
 
   return (
-    <div className="grid h-fit gap-8">
+    <div className="grid h-fit gap-4">
       <Typography variant="h1">Tic Tac Toe</Typography>
       <Typography variant="h2">
         You will play against a powerful AI! Can you beat it?
